2f8db97560f9c348913780709f3bc89f70aa1b0e
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-03-26 15:45:39 -0700

    deploy under object instead of reosurce account

--- aptos/simple/sh_scripts/publish.sh
+++ aptos/simple/sh_scripts/publish.sh
@@ -10,18 +10,29 @@ PUBLISHER_PROFILE=testnet-profile-1
 
 # Resource account seed is the seed used to derive the resource account address
 # It can be any string, but it should be unique for each resource account
-RESOURCE_ACCOUNT_SEED=resource-account-seed-2
+# RESOURCE_ACCOUNT_SEED=resource-account-seed-2
 
 PUBLISHER_ADDR=0x$(aptos config show-profiles --profile=$PUBLISHER_PROFILE | grep 'account' | sed -n 's/.*"account": \"\(.*\)\".*/\1/p')
 
-RESOURCE_ACCOUNT_ADDR=0x$(aptos account derive-resource-account-address \
-  --address $PUBLISHER_ADDR \
-  --seed $RESOURCE_ACCOUNT_SEED \
-  | jq -r '.Result')
+echo $PUBLISHER_ADDR
 
-aptos move create-resource-account-and-publish-package \
-  --address-name $PUBLISHER_ADDR\
-  --seed $RESOURCE_ACCOUNT_SEED \
-	--assume-yes \
+# RESOURCE_ACCOUNT_ADDR=0x$(aptos account derive-resource-account-address \
+#   --address $PUBLISHER_ADDR \
+#   --seed $RESOURCE_ACCOUNT_SEED \
+#   | jq -r '.Result')
+
+# aptos move create-resource-account-and-publish-package \
+#   --address-name $PUBLISHER_ADDR \
+#   --seed $RESOURCE_ACCOUNT_SEED \
+# 	--assume-yes \
+#   --profile $PUBLISHER_PROFILE \
+#   --named-addresses simple_todo_list_addr=$RESOURCE_ACCOUNT_ADDR
+
+aptos move compile \
+  --named-addresses simple_todo_list_addr=$PUBLISHER_ADDR
+
+aptos move create-object-and-publish-package \
   --profile $PUBLISHER_PROFILE \
-  --named-addresses simple_todo_list_addr=$RESOURCE_ACCOUNT_ADDR
+  --address-name simple_todo_list_addr \
+  --named-addresses my_module=$PUBLISHER_ADDR \
+	--assume-yes
