7912cf06a65ed20bcf4aa536012a69eafdffdbad
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-05-06 17:46:33 +0800

    deploy to testnet

--- friend-tech/aptos/move/sh_scripts/deploy.sh
+++ friend-tech/aptos/move/sh_scripts/deploy.sh
@@ -11,8 +11,8 @@ PUBLISHER_PROFILE=testnet-profile-1
 PUBLISHER_ADDR=0x$(aptos config show-profiles --profile=$PUBLISHER_PROFILE | grep 'account' | sed -n 's/.*"account": \"\(.*\)\".*/\1/p')
 
 OUTPUT=$(aptos move create-object-and-publish-package \
-  --address-name launchpad_addr \
-  --named-addresses launchpad_addr=$PUBLISHER_ADDR \
+  --address-name friend_tech_addr \
+  --named-addresses friend_tech_addr=$PUBLISHER_ADDR \
   --profile $PUBLISHER_PROFILE \
 	--assume-yes)
 
--- friend-tech/aptos/move/sh_scripts/issue_key_and_buy_key.sh
+++ friend-tech/aptos/move/sh_scripts/issue_key_and_buy_key.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-echo "##### Running move script to create and mint some FAs in 1 tx #####"
+echo "##### Running move script to issue key and buy some keys in 1 tx #####"
 
 CONTRACT_ADDRESS=$(cat contract_address.txt)
 
 # Need to compile the package first
 aptos move compile \
-  --named-addresses launchpad_addr=$CONTRACT_ADDRESS
+  --named-addresses friend_tech_addr=$CONTRACT_ADDRESS
 
 # Profile is the account you used to execute transaction
 # Run "aptos init" to create the profile, then get the profile name from .aptos/config.yaml
@@ -18,4 +18,4 @@ SENDER_PROFILE=testnet-profile-1
 aptos move run-script \
 	--assume-yes \
   --profile $SENDER_PROFILE \
-  --compiled-script-path build/launchpad/bytecode_scripts/create_and_mint_some_fas.mv
+  --compiled-script-path build/friend-tech/bytecode_scripts/issue_key_and_buy_key.mv
