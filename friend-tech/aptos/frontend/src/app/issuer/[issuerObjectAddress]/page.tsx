5aeeb64bd8f593660bb922a6c60a1e205066b624
J <129789810+0xaptosj@users.noreply.github.com>
2024-08-16 17:44:48 -0700

    J/update aptos friend using cad (#25)
    * replace friend tech with cad
    
    * Update README.MD

--- friend-tech/aptos/frontend/src/app/issuer/[issuerObjectAddress]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Issuer } from "@/components/Issuer";
-
-type Props = {
-  params: { issuerObjectAddress: `0x${string}` };
-};
-
-export default async function Page({ params: { issuerObjectAddress } }: Props) {
-  return <Issuer issuerObjectAddress={issuerObjectAddress} />;
-}
