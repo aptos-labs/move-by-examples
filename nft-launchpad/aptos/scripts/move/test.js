862e62bbf2757e1f085328c262778284673cbe21
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-08-16 17:31:14 -0700

    add nft launchpad using cad

---
+++ nft-launchpad/aptos/scripts/move/test.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const cli = require("@aptos-labs/ts-sdk/dist/common/cli/index.js");
+
+async function test() {
+  const move = new cli.Move();
+
+  await move.test({
+    packageDirectoryPath: "move",
+    namedAddresses: {
+      launchpad_addr: "0x100",
+      minter: "0x101",
+    },
+  });
+}
+test();
