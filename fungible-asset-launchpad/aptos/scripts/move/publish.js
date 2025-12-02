db67b1cd5300776640dd5bc6eb637288cb1a6f85
iskysun96 <iskysun96@gmail.com>
2025-12-01 16:32:15 -0800

    fixed minted asset image not loading error

--- fungible-asset-launchpad/aptos/scripts/move/publish.js
+++ fungible-asset-launchpad/aptos/scripts/move/publish.js
@@ -3,7 +3,7 @@ const fs = require("node:fs");
 const yaml = require("js-yaml");
 const cli = require("@aptos-labs/ts-sdk/dist/common/cli/index.js");
 
-const config = yaml.load(fs.readFileSync("./.aptos/config.yaml", "utf8"));
+const config = yaml.load(fs.readFileSync("./move/.aptos/config.yaml", "utf8"));
 const accountAddress = config["profiles"][`${process.env.PROJECT_NAME}-${process.env.VITE_APP_NETWORK}`]["account"];
 
 async function publish() {
